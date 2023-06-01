9e9ccf867be3c1739186ac3f53ac70f76bb111c5
security-man <zac.laker@crowncommercial.gov.uk>
2023-06-01 14:21:45 +0100

    Fixed keygeneration function

--- RSA.py
+++ RSA.py
@@ -9,86 +9,53 @@
 # 7) P = C^(d) mod n
 # PUBLIC key = (n,e), PRIVATE key = (n,d)
 
-import math
-
-# 1) Generate two primes, P and Q
-
 # generate prime number that is greater than specified int input
 def prime(greater_than):
     prime = greater_than
     i = 2
     while i < prime:
-        print(str(prime) + ' mod ' + str(i) + ' = ' + str(prime % i))
         if prime % i == 0:
-            print(str(prime) + " is not prime, divisible by " + str(i))
             prime = prime + 1
             i = 2
         else:
             i = i + 1
-    print(str(prime) + " is prime!")
     return prime
 
-print('Prime P generation ...')
-print('')
-P = prime(3)
-print('')
-print('Prime Q generation ...')
-print('')
-Q = prime(11)
-
-
-# 2) generate PUBLIC key n, n=P*Q
-
-print('')
-n=P*Q
-print('PUBLIC key n = ' + str(P) + ' * ' + str(Q) + ' = ' + str(n))
-
-# 3) generate psi(n) = (P-1)*(Q-1)
-
-print('')
-psi=(P-1)*(Q-1)
-print('psi(n) = ' + str(psi))
-
-# 4) generate e, 1 < e < psi(n), e is an integer and coprime to psi(n)
-
+# generate list of values that are coprime to input value
 def coprime(value):
-    coprime = 3
-    i = 3
-    print('Coprime e generation ...')
+    coprime = 2
+    coprime_list = []
+    i = 2
     while coprime < value:
-        if coprime % i != 0 or value % i != 0:
-            print(str(coprime) + ' mod ' + str(i) + ' = ' + str(coprime % i) + ', ' + str(value) + ' mod ' + str(i) + ' = ' + str(value % i))
-            i = i + 1
-            if i > coprime:
-                i = 3
+        for i in range(i,coprime-1):
+            if coprime % i == 0 and value % i == 0:
+                i = 2
                 coprime = coprime + 1
-        else :
-            print(str(coprime) + ' and ' + str(value) + ' are coprime, common factor = ' + str(i))
-            break
-    return coprime
+        if value % coprime != 0:
+            coprime_list.append(coprime)
+        i = 2
+        coprime = coprime + 1
+    return coprime_list
 
-print('')
-e = coprime(psi)
+# generate private key based on minimum values for P and Q, initial primes, using coprime function
+def keygeneration(a,b):
+    P = prime(a)
+    Q = prime(b)
+    n = P*Q
+    psi = (P-1)*(Q-1)
+    coprimelist = coprime(psi)
+    coprimecounter = 1
+    d = 0
+    e = 0
+    while coprimecounter < len(coprimelist):
+        e = coprimelist[coprimecounter]
+        for k in range(10):
+            if (((k*psi)+1) % e) == 0:
+                d = (int) (((k*psi)+1)/e)
+                print('P = ' + str(P) + ', Q = ' + str(Q) + ', n = ' + str(n) + ', Psi = ' + str(psi) + ', e = ' + str(e) + ', d = ' + str(d))
+                coprimecounter = len(coprimelist)
+                break
+        coprimecounter = coprimecounter + 1
+    return P,Q,n,psi,e,d
 
-e = 7
-
-# 5) generate d, (d*e) mod psi = 1, d is integer (iterate until d satisfies equation)
-
-def privatekey(psi,e):
-    d = 1
-    k = ((e*d) % psi)
-    while k != 1:
-        d = d + 1
-        k = ((e*d) % psi)
-    return d
-
-print('psi = ' + str(psi) + ' e = ' + str(e))
-
-d = privatekey(psi,e)
-
-print('')
-print('d = ' + str(d))
-
-# 6) C= P^(e) mod n
-# 7) P = C^(d) mod n
-# PUBLIC key = (n,e), PRIVATE key = (n,d)
+P,Q,n,psi,e,d = keygeneration(32,43)
